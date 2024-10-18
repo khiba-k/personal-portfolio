0cf6384ae92c682bc204a3ffa38ffba1f0bba634
khiba-k <khibakoenane@gmail.com>
2024-10-18 18:10:42 +0200

    revamped

---
+++ revamped/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <section id="header"></section>
+      <section id="intro"></section>
+      <section id="projects"></section>
+      <section id="footer"></section>
+    </div>
+  );
+}
